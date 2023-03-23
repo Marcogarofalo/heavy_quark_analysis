dc0d1764e90c1f4844bbdbc446240d2b83b6689f
Marcogarofalo <marcogarofalo31@gmail.com>
2023-03-23 17:36:37 +0100

    indices

--- functions_heavy_quarks.cpp
+++ functions_heavy_quarks.cpp
@@ -6,13 +6,57 @@ double lhs_function_heavy_quarks_eg(int j, double**** in, int t, struct fit_type
     return r;
 }
 
-double lhs_function_f_PS(int j, double**** in, int t, struct fit_type fit_info){
-    int id=fit_info.corr_id[0];
-    double corr=in[j][id][t][0];
-    double M=fit_info.ext_P[0][j];
-    double mu1=fit_info.ext_P[1][j];
-    double mu2=fit_info.ext_P[2][j];
 
-    double me=sqrt(corr*2*M/(exp(-t*M)+exp(-(fit_info.T-t)*M)));
-    return (mu1+mu2)*me/(M*sinh(M));
+mass_index::mass_index(int nmus)
+{
+     int k1, k2,r1,r2,i;
+     nk=nmus;
+
+     table=(int****) malloc(sizeof(int***)*nk);
+     for (k1=0;k1<nk;k1++){
+     	table[k1]=(int***) malloc(sizeof(int**)*2);
+     		for (r1=0;r1<2;r1++){
+     			table[k1][r1]=(int**) malloc(sizeof(int*)*(k1+1));
+     			for (k2=0;k2<=k1;k2++){
+	     			table[k1][r1][k2]=(int*) malloc(sizeof(int)*2);		
+			}
+		}
+     }
+
+     tot_mu_r=0;
+     for (k1=0;k1<nk;k1++)
+     for (r1=0;r1<2;r1++)
+     for (k2=0;k2<=k1;k2++)
+     for (r2=0;r2<2;r2++){
+    	table[k1][r1][k2][r2]=i;
+    	tot_mu_r++;
+    }
+
+
 }
+
+
+mass_index::~mass_index()
+{
+    
+     for (int k1=0;k1<nk;k1++){
+     		for (int r1=0;r1<2;r1++){
+     			for (int k2=0;k2<=k1;k2++){
+                    free(table[k1][r1][k2]);
+			}
+            free(table[k1][r1]);
+		}
+        free(table[k1]);
+     }
+     free(table);
+
+}
+
+id_contraction::id_contraction(int nmus, int ngamma): mass_index(nmus){
+    Ngamma=ngamma;
+};
+int id_contraction::return_id(int k1, int k2, int r1, int r2, int ig, int is){
+    return table[k1][r1][k2][r2]+tot_mu_r*(ig+Ngamma*(is) );
+
+}
+
--- functions_heavy_quarks.hpp
+++ functions_heavy_quarks.hpp
@@ -2,6 +2,22 @@
 #define functions_heavy_quarks_H
 #include "non_linear_fit.hpp"
 
-double lhs_function_heavy_quarks_eg(int j, double ****in, int t, struct fit_type fit_info);
-double lhs_function_f_PS(int j, double ****in, int t, struct fit_type fit_info);
+double lhs_function_heavy_quarks_eg(int j, double**** in, int t, struct fit_type fit_info);
+
+class mass_index {
+public:
+    int nk;
+    int**** table;
+    int tot_mu_r;
+    mass_index(int nmus);
+    ~mass_index();
+};
+
+class id_contraction: mass_index {
+public:
+    id_contraction(int nmus, int ngamma);
+    int return_id(int k1, int k2, int r1, int r2, int ig, int is);
+    int Ngamma;
+};
+
 #endif
