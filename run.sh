97669e10b5f058358f7409577fdf0a2f4cce6283
Marcogarofalo <marcogarofalo31@gmail.com>
2023-12-01 16:27:53 +0100

    more runs

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_ins48_ins64_th6.dat -bin 1 jack
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th1_t56_44.dat -bin 1 jack
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th3_t56_44.dat -bin 1 jack
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th5_t56_44.dat -bin 1 jack
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th6_t56_44.dat -bin 1 jack
-./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th6_t56_44_nobin.dat -bin 1 jack
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th7_t56_44.dat -bin 1 jack
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th9_t56_44.dat -bin 1 jack
+
+#./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th6_t56_44_nobin.dat -bin 1 jack
 
 #./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_ins48_ins64_th0.dat -bin 1 jack || exit 1
 #./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_ins48_ins64_th6.dat -bin 1 jack || exit 1
