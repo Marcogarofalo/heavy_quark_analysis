02329e0f951c365c41955e390da671dc75e2b6b0
Marcogarofalo <marcogarofalo31@gmail.com>
2023-12-04 18:15:14 +0100

    B96 odd theta

--- run.sh
+++ run.sh
@@ -7,6 +7,16 @@
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th7_t56_44.dat -bin 1 jack
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th9_t56_44.dat -bin 1 jack
 
+
+
+
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th1_t56_44.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th3_t56_44.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th5_t56_44.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th7_t56_44.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th9_t56_44.dat -bin 1 jack || exit 1
+
+
 #./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th6_t56_44_nobin.dat -bin 1 jack
 
 #./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_ins48_ins64_th0.dat -bin 1 jack || exit 1
