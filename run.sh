991649e46bff44d7c2d35936ee44296d06f95367
Marcogarofalo <marcogarofalo31@gmail.com>
2023-12-13 15:12:21 +0100

    D runs

--- run.sh
+++ run.sh
@@ -9,13 +9,18 @@
 
 
 
-
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th1_t56_44.dat -bin 1 jack || exit 1
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th3_t56_44.dat -bin 1 jack || exit 1
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th5_t56_44.dat -bin 1 jack || exit 1
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th7_t56_44.dat -bin 1 jack || exit 1
 ./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.96_th9_t56_44.dat -bin 1 jack || exit 1
 
+
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cD211.054.96_th1_t78_62.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cD211.054.96_th3_t78_62.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cD211.054.96_th5_t78_62.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cD211.054.96_th7_t78_62.dat -bin 1 jack || exit 1
+./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cD211.054.96_th9_t78_62.dat -bin 1 jack || exit 1
 
 #./inclusive_decay -p ../../../heavy_quarks/data_Inc/ cB211.072.64_th6_t56_44_nobin.dat -bin 1 jack
 
