09579a3550829bd2978e92e25a7bd611a37def23
Marcogarofalo <marcogarofalo31@gmail.com>
2024-05-28 10:53:03 +0200

    adding E ensemble

--- fit_all_inclusive_Ds.cpp
+++ fit_all_inclusive_Ds.cpp
@@ -203,6 +203,28 @@ int main(int argc, char** argv) {
 
     int Ntheta_D96 = files.size() - Ntheta_B64 - Ntheta_B96 - Ntheta_C80;
 
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th1_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th2_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th3_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th4_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th5_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th6_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th7_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th8_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th9_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+    mysprintf(namefile, NAMESIZE, "%s/%s_cE211.044.112_th9.5_t91_72.dat", argv[2], argv[1]);
+    files.emplace_back(namefile);
+
+    int Ntheta_E112 = files.size() - Ntheta_B64 - Ntheta_B96 - Ntheta_C80 - Ntheta_D96;
 
     std::vector<int> all_en(files.size());
     for (int e = 0; e < files.size(); e++) {
@@ -228,7 +250,7 @@ int main(int argc, char** argv) {
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////
-    // fits 
+    // fits sigma4
     /////////////////////////////////////////////////////////////////////////////////////////////////
     std::vector<std::string>th_map = { "1","2","3","4","5","6","7","8","9","9.5" };
     error(th_map.size() != Ntheta_B64, 1, "check the thetas", "");
@@ -240,11 +262,12 @@ int main(int argc, char** argv) {
         fit_info.Nvar = 3;
         fit_info.Njack = Njack;
 
-        fit_info.myen.resize(4);
+        fit_info.myen.resize(5);
         fit_info.myen[0] = th;
         fit_info.myen[1] = fit_info.myen[0] + Ntheta_B64;
         fit_info.myen[2] = fit_info.myen[1] + Ntheta_B96;
         fit_info.myen[3] = fit_info.myen[2] + Ntheta_C80;
+        fit_info.myen[4] = fit_info.myen[3] + Ntheta_E112;
 
         fit_info.entot = fit_info.myen.size() * fit_info.N;
         fit_info.malloc_x();// Nvar, entot, Njack
@@ -261,7 +284,7 @@ int main(int argc, char** argv) {
             }
         }
         fit_info.function = rhs_const;
-        fit_info.corr_id = { 57 };//
+        fit_info.corr_id = { 69 };//
         char namefit[NAMESIZE];
         mysprintf(namefit, NAMESIZE, "dGamma_th%s_const", th_map[th].c_str());
         fit_result dGamma_th = fit_all_data(argv, jackall, lhs_identity, fit_info, namefit);
@@ -274,7 +297,7 @@ int main(int argc, char** argv) {
 
         dGamma_th.clear();
     }
-    double **Cov = myres->comp_cov(Ntheta_B64, fit_result_dG);
+    double** Cov = myres->comp_cov(Ntheta_B64, fit_result_dG);
     char namecov[NAMESIZE];
     mysprintf(namecov, NAMESIZE, "%s/dGamma_const_corr.dat", argv[3]);
     FILE* fcov = open_file(namecov, "w+");
@@ -282,11 +305,63 @@ int main(int argc, char** argv) {
         for (int j = 0;j < Ntheta_B64;j++) {
             fprintf(fcov, "%-22.4g", Cov[i][j] / sqrt(Cov[i][i] * Cov[j][j]));
         }
-        fprintf(fcov,"\n");
+        fprintf(fcov, "\n");
     }
     fclose(fcov);
     free_2(Ntheta_B64, Cov);
     free_2(Ntheta_B64, fit_result_dG);
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    // fits sigma4 Z0 part
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    error(th_map.size() != Ntheta_B64, 1, "check the thetas", "");
+    fit_result_dG = malloc_2<double>(Ntheta_B64, Njack);
+    for (int th = 0;th < Ntheta_B64;th++) {
+        fit_type fit_info;
+        fit_info.Npar = 1;
+        fit_info.N = 1;
+        fit_info.Nvar = 3;
+        fit_info.Njack = Njack;
+
+        fit_info.myen.resize(5);
+        fit_info.myen[0] = th;
+        fit_info.myen[1] = fit_info.myen[0] + Ntheta_B64;
+        fit_info.myen[2] = fit_info.myen[1] + Ntheta_B96;
+        fit_info.myen[3] = fit_info.myen[2] + Ntheta_C80;
+        fit_info.myen[4] = fit_info.myen[3] + Ntheta_E112;
+
+        fit_info.entot = fit_info.myen.size() * fit_info.N;
+        fit_info.malloc_x();// Nvar, entot, Njack
+        int count = 0;
+        for (int n = 0;n < fit_info.N;n++) {
+            // for (int e = 0;e < fit_info.myen.size();e++) {
+            for (int e : fit_info.myen) {
+                for (int j = 0;j < Njack;j++) {
+                    fit_info.x[0][count][j] = pow(jackall.en[e].jack[58][j], 2);
+                    fit_info.x[1][count][j] = pow(jackall.en[e].header.thetas[0] * M_PI / (jackall.en[e].header.L * jackall.en[e].jack[58][j] * 1000.0 / hbarc), 2);// GeV
+                    fit_info.x[2][count][j] = jackall.en[e].header.L;
+                }
+                count++;
+            }
+        }
+        fit_info.function = rhs_const;
+        fit_info.corr_id = { 70 };//
+        char namefit[NAMESIZE];
+        mysprintf(namefit, NAMESIZE, "dGamma_th%s_part_const", th_map[th].c_str());
+        fit_result dGamma_th = fit_all_data(argv, jackall, lhs_identity, fit_info, namefit);
+        fit_info.band_range = { 0,0.0081 };
+        print_fit_band(argv, jackall, fit_info, fit_info, namefit, "afm", dGamma_th, dGamma_th, 0, fit_info.myen.size() - 1, 0.001);
+
+        fit_info.restore_default();
+        for (int j = 0;j < Njack;j++)
+            fit_result_dG[th][j] = dGamma_th.P[0][j];
+
+        dGamma_th.clear();
+    }
+    Cov = myres->comp_cov(Ntheta_B64, fit_result_dG);
+    mysprintf(namecov, NAMESIZE, "%s/dGamma_const_corr.dat", argv[3]);
+    
+    free_2(Ntheta_B64, Cov);
+    free_2(Ntheta_B64, fit_result_dG);
 
 }
